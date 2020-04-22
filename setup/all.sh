41f16cc2560515fce751ce29d80480bcf2a5c347
Paul Richards <paulware@hotmail.com>
2020-04-21 21:14:42 -0500

    connect.sh

--- setup/all.sh
+++ setup/all.sh
@@ -3,9 +3,10 @@ chmod 777 *.*
 cd /boot/
 # Copy setup files to /boot 
 apt-get update
+apt-get install expect -y
 apt-get install python-pexpect -y
 apt-get install bluetooth bluez blueman -y
-apt-get install motion -y
+#apt-get install motion -y
 # Note: raspberry pi camera is preferred due to less lag, using mjpg-streamer
 # For raspberry pi camera 
 #   enable camera using raspi-config
@@ -27,4 +28,6 @@ apt-get install motion -y
 #systemctl start mjpg_streamer.service
 #ifup wlan0
 echo "Done in setup, Using the wifi icon on desktop:turn wifi on, and set wifi country"
-echo "Then reboot"
+echo "Then reboot to turn camera on"
+echo "Place sd card in pizero-w, power up, connect to access point"
+echo "Turn on the bluetooth device and then run ./connect.sh"
